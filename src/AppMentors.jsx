b394ff452890eeae7d3e9e869121fa4504351311
ieongeun <ieongeun0@gmail.com>
2023-02-02 16:51:43 +0900

    컴포넌트 깔끔하게 관리하기

--- src/AppMentors.jsx
+++ src/AppMentors.jsx
@@ -1,19 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import AppMentor from './AppMentor';
 
-export default function AppMentors(props) {
-  const [person, setPerson] = useState({
-    name: '엘리',
-    title: '개발자',
-    mentors: [{
-      name: '밥',
-      title: '시니어개발자',
-    },
-    {
-      name: '존',
-      title: '꿀꿀박사',
-    }]
-  });
+export default function AppMentors() {
+  const [person, setPerson] = useState(initialPerson);
+  function handleUpdate(){
+    const prev = prompt(`누구의 이름을 바꿀래?`);
+    const current = prompt(`뭘로?`);
+    setPerson((person)=> ({...person, mentors : person.mentors.map(
+      (mentor)=> mentor.name===prev? {...mentor,name:current}:mentor
+      )
+  })
+    )
+  }
+  function handleAdd() {
+    const name = prompt(`이름은 뭐야?`);
+    const title = prompt(`직업은 뭐야?`);
+    setPerson((person) =>  ({...person ,mentors :[...person.mentors,{name,title}]}))
+  }
+  function hadleDlete() {
+    const name = prompt(`누구를 삭제할거야?(이름정확히 입력해줘)`);
+    setPerson((person)=>({...person, mentors : (person.mentors.filter((item)=> item.name!== name))}))
+  }
   return (
     <div>
       <ul>
@@ -22,27 +29,25 @@ export default function AppMentors(props) {
         ))}
       </ul>
       <button
-        onClick={() => {
-          const prev = prompt(`누구의 이름을 바꿀래?`);
-          const current = prompt(`뭘로?`);
-          setPerson((person)=> ({...person, mentors : person.mentors.map(
-            (mentor)=> mentor.name===prev? {...mentor,name:current}:mentor
-            )
-        })
-          )
-        }}
+        onClick={handleUpdate}
         >
         멘토 이름 바꾸기
       </button>
-      <button onClick={()=>{
-        const name = prompt(`이름은 뭐야?`);
-        const title = prompt(`직업은 뭐야?`);
-        setPerson((person) =>  ({...person ,mentors :[...person.mentors,{name,title}]}))
-      }}>멘토추가</button>
-      <button onClick={()=>{
-        const name = prompt(`누구를 삭제할거야?(이름정확히 입력해줘)`);
-        setPerson((person)=>({...person, mentors : (person.mentors.filter((item)=> item.name!== name))}))
-      }}>멘토삭제</button>
+      <button onClick={handleAdd}>멘토추가</button>
+      <button onClick={hadleDlete}>멘토삭제</button>
     </div>
   );
 }
+
+const initialPerson = {
+  name: '엘리',
+  title: '개발자',
+  mentors: [{
+    name: '밥',
+    title: '시니어개발자',
+  },
+  {
+    name: '존',
+    title: '꿀꿀박사',
+  }]
+};
