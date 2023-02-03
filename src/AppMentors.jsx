38ef06151a414073eb1c9dd4e5caaa8f4f10811e
ieongeun <ieongeun0@gmail.com>
2023-02-03 16:16:56 +0900

    Immer 사용해보기

--- src/AppMentors.jsx
+++ src/AppMentors.jsx
@@ -1,22 +1,30 @@
 import React, { useEffect, useReducer, useState } from 'react';
 import AppMentor from './AppMentor';
-import personReducer from './reducer/person-reducer';
+import {useImmer} from 'use-immer';
+
 
 export default function AppMentors() {
-  const [person, dispatch] = useReducer(personReducer, initialPerson);
+  const [person, updatePerson] = useImmer(initialPerson);
   function handleUpdate(){
     const prev = prompt(`누구의 이름을 바꿀래?`);
     const current = prompt(`뭘로?`);
-    dispatch({prev,current,type:'update'});
+    updatePerson((person)=>{
+      const mentor = person.mentors.find((m)=> m.name === prev);
+      mentor.name = current;
+    })
   }
   function handleAdd() {
     const name = prompt(`이름은 뭐야?`);
     const title = prompt(`직업은 뭐야?`);
-    dispatch({name,title,type:'add'});
+    updatePerson((person)=> {
+      person.mentors.push({name, title})
+    })
   }
   function hadleDlete() {
     const name = prompt(`누구를 삭제할거야?(이름정확히 입력해줘)`);
-    dispatch({name,type:'delete'});
+    updatePerson((person)=> {
+      person.mentors = person.mentors.filter((mentor)=> mentor.name!==name)
+    })
   }
   return (
     <div>
