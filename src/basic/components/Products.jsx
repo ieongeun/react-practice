ab86f248d5eaf05e71c3e7280bf87e5ce679a18f
ieongeun <ieongeun0@gmail.com>
2023-02-10 17:15:38 +0900

    feat : handle loading and error

--- src/basic/components/Products.jsx
+++ src/basic/components/Products.jsx
@@ -1,26 +1,34 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Products() {
+  const [loading,setLoading] = useState(false);
+  const [error,setError] = useState();
   const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
-  const handleChange = () => setChecked((prev)=> !prev);
+  const handleChange = () => setChecked((prev) => !prev);
 
   useEffect(() => {
-    fetch(`data/${checked? 'sale_' : ''}products.json`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
+    setLoading(true);
+    fetch(`data/${checked ? 'sale_' : ''}products.json`)
+    .then((res) => res.json())
+    .then((data) => {
         setProducts(data);
-      });
-    return () => {
-      console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
-    };
+      })
+    .catch(()=> setError(true))
+    .finally(()=> setLoading(false))
   }, [checked]);
 
+  if(loading) return <p>⛄⛄⛄⛄로딩중이다</p>;
+  if(error) return <p>⚠️⚠️⚠️⚠️⚠️⚠️에러다</p>;
   return (
     <>
-    <input id="sale" type="checkbox" value={checked} onChange={handleChange}/>
-    <label htmlFor="sale">HOT SALE</label>
+      <input
+        id='checkbox'
+        type='checkbox'
+        value={checked}
+        onChange={handleChange}
+      />
+      <label htmlFor='checkbox'>Show Only 🔥 Sale</label>
       <ul>
         {products.map((product) => (
           <li key={product.id}>
