285a810d407e9386d44437c6dd2f56c9e970a503
ieongeun <ieongeun0@gmail.com>
2023-02-16 16:03:00 +0900

    쓸데없는 익명함수 제거

--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
 export default function Todo({todo:{id, text, status}, onDelete, onUpdate}) {
-    const handleClick = (key) => onDelete(key);
-    const handleChange = (key) => onUpdate(key);
+    const handleClick = () => onDelete(id);
+    const handleChange = () => onUpdate(id);
     return (
         <li key={id}>
-            <input type="checkbox" id={id} checked={status} onChange={()=>handleChange(id)}/>
+            <input type="checkbox" id={id} checked={status} onChange={handleChange}/>
             <label htmlFor={id}>{text}</label>
-            <button onClick={()=>handleClick(id)}>삭제</button>
+            <button onClick={handleClick}>삭제</button>
         </li>
     );
 }
 
+// 아 바보 이 하나의 컴포넌트가 하나지 그럼 ()=> 이렇게할일도 없었잖아 ㄱ치 
