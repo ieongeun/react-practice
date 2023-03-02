b7ea5bf8cf92f03f2d54132eefca82ad79a35ae6
ieongeun <ieongeun0@gmail.com>
2023-03-02 20:06:37 +0900

    chore: Navber 오타

--- router/src/components/Navbar.jsx
+++ router/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useReducer, useState } from 'react';
 import { Link } from 'react-router-dom';
-export default function Navber(){
+export default function Navbar(){
   return (
     <nav>
         <Link to='/'>Home</Link>
